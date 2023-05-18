7f08e251eae8158179e2f80aca2fbe1526b2b9dc
Isaac <uniqucode@gmail.com>
2023-05-18 13:58:25 +0100

    Ready for deployment

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose, { mongo } from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`MongoDB Connected: ${connect.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
